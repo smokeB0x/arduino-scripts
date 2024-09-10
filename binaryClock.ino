f5156006f0ab285daa61a75ab69c30ff7bff8be0
smokeB0x <32621767+smokeB0x@users.noreply.github.com>
2024-09-10 13:03:02 +0200

    Update binaryClock.ino

--- binaryClock.ino
+++ binaryClock.ino
@@ -2,78 +2,130 @@
 Script for a binary clock of LEDs - 24h
 Microcontroller: Arduino NANO
 Created: 09.09.24
-Version: 1.0
+Last updated: 10.09.24
+Version: 1.3
 */
 
-//Not shure if I want to use this lib or not
-//#include <TimeLib.h>
+unsigned long previousMillis = 0;  // Store the last time minutes were updated
+const unsigned long interval = 60000;  // 60000 milliseconds = 60 seconds
+int minutes = 45;
+int hours = 23;
 
-// Define the output pins of the LEDs and the buttons
-int hourOne = ;
-int hourTwo = ;
-int hourFour = ;
-int hourEight = ;
-int hourSixteen = ;
+//Declare buttons
+int buttonDecrease = A4;
+int buttonIncrease = A5;
 
-int minOne = ;
-int minTwo = ;
-int minFour = ;
-int minEight = ;
-int minSixteen = ;
-int minThirtyTwo = ;
+//Declare hour LEDs
+int hourOne = 8;
+int hourTwo = 9;
+int hourFour = 10;
+int hourEight = 11;
+int hourSixteen = 12;
 
-int buttonIncrease = ;
-int buttonDecrease = ;
+//Declare minute LEDs
+int minOne = 2;
+int minTwo = 3;
+int minFour = 4;
+int minEight = 5;
+int minSixteen = 6;
+int minThirtyTwo = 7;
 
-//Define the output of the LEDs to be OUTPUT and the buttons to be INPUT
 void setup() {
-
-pinMode(hourOne, OUTPUT);
-pinMode(hourTwo, OUTPUT);
-pinMode(hourFour, OUTPUT);
-pinMode(hourEight, OUTPUT);
-pinMode(hourSixteen, OUTPUT);
-
-pinMode(minOne, OUTPUT);
-pinMode(minTwo, OUTPUT);
-pinMode(minFour, OUTPUT);
-pinMode(minEight, OUTPUT);
-pinMode(minSixteen, OUTPUT);
-pinMode(minThirtyTwo, OUTPUT);
-
-pinMode(buttonIncrease, INPUT);
-pinMode(buttonDecrease, INPUT);
-
-Serial.begin(9600);
+  pinMode(buttonDecrease, INPUT);
+  pinMode(buttonIncrease, INPUT);
+  
+  pinMode (hourOne, OUTPUT);
+  pinMode (hourTwo, OUTPUT);
+  pinMode (hourFour, OUTPUT);
+  pinMode (hourEight, OUTPUT);
+  pinMode (hourSixteen, OUTPUT);
+  
+  pinMode (minOne, OUTPUT);
+  pinMode (minTwo, OUTPUT);
+  pinMode (minFour, OUTPUT);
+  pinMode (minEight, OUTPUT);
+  pinMode (minSixteen, OUTPUT);
+  pinMode (minThirtyTwo, OUTPUT);
+  
+  Serial.begin(9600); //Serial for testin purposes
 }
 
 /*
-TIME KEEPER FUNCTION
-A function that keeps the time to a minute
-Ensure that time does not go below 0 or above 23:59
+LOOP FUNCTION
+First section keeps the time to a minute
+Ensures that time does not go below 0 or above 23:59
+Also included two buttons that increase or decrease the minute by 1
+Lastly it lights up the LEDs based on the values in hour and minutes
 */
 
-int timeKeeper(){
-
-  unsigned long seconds = (millis() / 1000) % 3600;
-
-  return seconds;
-}
 
 void loop() {
-//Print the time every 5 seconds as a test
-Serial.println(timeKeeper());
-delay(5000);
+  unsigned long currentMillis = millis();
+  
+  // Check if 60 seconds have passed
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;  // Update the time
+    minutes++;  // Increment the minutes
 
+    // When minutes reach 60, reset to 0 and increment hours
+    if (minutes >= 60) {
+      minutes = 0;
+      hours++;
+      
+      // Reset hours after 24 (for 24-hour clock)
+      if (hours >= 24) {
+        hours = 0;
+      }
+    }
+  }
+
+  // Check button presses
+  if (digitalRead(buttonIncrease) == HIGH) {
+    minutes++;
+    if (minutes >= 60) {
+      minutes = 0;
+      hours++;
+      if (hours >= 24) {
+        hours = 0;
+      }
+    }
+    delay(200);  // Debouncing delay
+  }
+  
+  if (digitalRead(buttonDecrease) == HIGH) {
+    if (minutes > 0) {
+      minutes--;
+    } else {
+      minutes = 59;
+      if (hours > 0) {
+        hours--;
+      } else {
+        hours = 23;
+      }
+    }
+    delay(200);  // Debouncing delay
+  }
+
+  // Print the time - for testing purposes
+  Serial.print("Hours: ");
+  Serial.print(hours);
+  Serial.print(" Minutes: ");
+  Serial.println(minutes);
+  delay(1000);
+  
+  //Binary representation of hours: use bitwise operations
+  digitalWrite(hourOne, (hours & 0b00001) ? HIGH : LOW);
+  digitalWrite(hourTwo, (hours & 0b00010) ? HIGH : LOW);
+  digitalWrite(hourFour, (hours & 0b00100) ? HIGH : LOW);
+  digitalWrite(hourEight, (hours & 0b01000) ? HIGH : LOW);
+  digitalWrite(hourSixteen, (hours & 0b10000) ? HIGH : LOW);
+  
+  // Binary representation of minutes: use bitwise operations
+  digitalWrite(minOne, (minutes & 0b000001) ? HIGH : LOW);
+  digitalWrite(minTwo, (minutes & 0b000010) ? HIGH : LOW);
+  digitalWrite(minFour, (minutes & 0b000100) ? HIGH : LOW);
+  digitalWrite(minEight, (minutes & 0b001000) ? HIGH : LOW); 
+  digitalWrite(minSixteen, (minutes & 0b010000) ? HIGH : LOW);
+  digitalWrite(minThirtyTwo, (minutes & 0b100000) ? HIGH : LOW); 
+  
 }
-
-/* TODO
-BUTTON PRESS FUNCTION
-Two button inputs
-Function to increase the time with 60 seconds if one button is pressed, and decrease it with 60 seconds if the other is pressed
-*/
-
-/* TODO
-LED POWER FUNCTION
-Function that powers the correct LEDs based on the value in the time keeper function
-*/
